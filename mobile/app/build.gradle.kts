b006d7f31c116c65ce9103911a537f29795138b2
sara-stojkov <sarastojkov6@gmail.com>
2026-04-25 19:53:33 +0200

    fix: fix dependencies and resources

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -34,13 +34,21 @@ android {
 }
 
 dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    implementation(libs.navigation.fragment)
-    implementation(libs.navigation.ui)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    dependencies {
+        implementation(libs.appcompat)
+        implementation(libs.material)
+        implementation(libs.activity)
+        implementation(libs.constraintlayout)
+        implementation(libs.navigation.fragment)
+        implementation(libs.navigation.ui)
+
+        // Google Maps
+        implementation("com.google.android.gms:play-services-maps:18.2.0")
+        // Location
+        implementation("com.google.android.gms:play-services-location:21.2.0")
+
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.ext.junit)
+        androidTestImplementation(libs.espresso.core)
+    }
 }
